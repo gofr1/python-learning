c0bddeb78a3656f04010ec580e1ffdf72e6dea54
Ilia Odintsov <gofr.one@gmail.com>
2020-02-19 23:19:04 +0100

    Task 3 solved

--- challenge/get_symbol_between.py
+++ challenge/get_symbol_between.py
@@ -8,18 +8,24 @@ contents = page_contents.split('<!--')
 parse_contents = dict()
 len_str = 7
 content_to_parse = contents[-1].split('\n')
+message = ''
 
-message = dict()
-
+i = 0
+before= ''
+after = ''
 for string in content_to_parse:
     i = 0
     while i < len(string) - len_str:
         str_check = string[i:i+len_str]
-        if str_check[:3] == str_check[:3].upper() and str_check[4:] == str_check[4:].upper() and str_check[3] == str_check[3].lower():
-            m = message.setdefault(str_check[3],0)
-            message[str_check[3]] +=1
-        i += len_str
-    # message += '\n'
+        before = string[i-1] 
+        after = string[i+len_str]
+        if (str_check[:3] == str_check[:3].upper() and 
+            str_check[4:] == str_check[4:].upper() and 
+            str_check[3] == str_check[3].lower() and
+            before == before.lower() and after == after.lower()
+        ):
+            # print("{} | {} {} {} | {}".format(before, str_check[:3], str_check[3], str_check[4:], after))
+            message += str_check[3]
+        i += 1
 
-{k: v for k, v in sorted(message.items(), key=lambda item: item[1], reverse=True)}
-
+print(message)
