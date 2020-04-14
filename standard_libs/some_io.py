8fd963769d90cfa4fd8ec96ed8e63c5e187f07c8
Ilia Odintsov <gofr.one@gmail.com>
2020-04-14 15:09:00 +0200

    IO sample and rearranging

---
+++ standard_libs/some_io.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def main():
+    # Output 
+    print("Hello there!")
+
+    # Input
+    colour = input("What is your favourite colour?")
+    print(colour)
+
+if __name__ == '__main__':
+    main()
