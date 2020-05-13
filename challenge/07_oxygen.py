1dce2665cfda385cce34789581c71791185d8af9
Ilia Odintsov <gofr.one@gmail.com>
2020-05-13 18:13:04 +0200

    Task 7 done

--- challenge/07_oxygen.py
+++ challenge/07_oxygen.py
@@ -2,7 +2,7 @@
 from bs4 import BeautifulSoup, SoupStrainer
 import requests
 from io import BytesIO
-
+from PIL import Image
 
 standard_url = "http://www.pythonchallenge.com/pc/def/"
 
@@ -18,6 +18,22 @@ for link in soup.find_all('img'):
 for picture_url in picture_urls:
     response = requests.get(f'{standard_url}{picture_url}')
 
-img = response.content
+    img = Image.open(BytesIO(response.content))
+    img_size = img.size
+    l = ''
+    y = img_size[1]//2
 
-byte_io = BytesIO(img)
+    for x in range(0,img_size[0],7):
+        r, b, g, a = img.getpixel((x, y))
+        if r==b==g:
+            l += chr(r)
+    
+    print(l)
+    
+    answer = l[(len(l)-1)//2:-1]
+    result = ''
+    for symbol in answer.split(','):
+        result += chr(int(symbol))
+    
+    print(result)
+
