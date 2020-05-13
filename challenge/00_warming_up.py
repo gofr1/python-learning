0b515a2b5a51051648027d0fd83f52adc78f254e
Ilia Odintsov <gofr.one@gmail.com>
2020-05-13 15:20:05 +0200

    First 6 tasks done

---
+++ challenge/00_warming_up.py
@@ -0,0 +1,4 @@
+# 0
+# http://www.pythonchallenge.com/pc/def/0.html
+
+print(2**38)
---
+++ challenge/07_smarty.py
@@ -0,0 +1,21 @@
+# 7
+import requests
+
+url = 'http://www.pythonchallenge.com/pc/def/oxygen.html'
+
+page_contents = requests.get(url).text[:]
+contents = page_contents.split('<!--')
+parse_contents = dict()
+
+for symbol in contents[-1]:
+    m = parse_contents.setdefault(symbol,0)
+    parse_contents[symbol] +=1
+
+message = ''
+for key, value in parse_contents.items():
+    message += key if value == 1 else ''
+
+print(message)
+
+
+
