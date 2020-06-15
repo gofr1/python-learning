32fc043ba8e01acb7e1667ab19062ad4702f0d3e
Ilia Odintsov <gofr.one@gmail.com>
2020-06-15 12:22:14 +0200

    Dash markdown

---
+++ visualization/dash_markdown.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+
+markdown_text = '''
+### Dash and Markdown
+A lot of text
+'''
+
+app.layout = html.Div([
+    dcc.Markdown(children=markdown_text),
+
+    html.Label('Multi-Select Dropdown'),
+    dcc.Dropdown(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': u'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value=['MTL', 'SF'],
+        multi=True
+    ),
+
+    html.Label('Radio Items'),
+    dcc.RadioItems(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': u'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value='MTL'
+    ),
+
+    html.Label('Checkboxes'),
+    dcc.Checklist(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': u'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value=['MTL', 'SF']
+    ),
+
+    html.Label('Text Box'),
+    dcc.Input(value='MTL', type='text'),
+])
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run_server()
