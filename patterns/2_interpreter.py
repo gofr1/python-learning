b3fe43df5653cc9c291993a71feace950db33425
Ilia Odintsov <gofr.one@gmail.com>
2020-07-02 08:09:31 +0200

    Gang of Four patterns part 1

---
+++ patterns/2_interpreter.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+class AbstractExpression():
+    @abstractmethod
+    def interpret(self):
+        pass
+
+class NonTerminalExpression(AbstractExpression):
+
+    def __init__(self, expression):
+        self._expression = expression
+
+    def interpret(self):
+        print('Non-terminal expression being interpreted...')
+        self._expression.interpret()
+
+class TerminalExpression(AbstractExpression):
+
+    def interpret(self):
+        print('Terminal expression being interpreted...')
+
+def main():
+    ast = NonTerminalExpression(NonTerminalExpression(TerminalExpression()))
+    ast.interpret()
+
+if __name__ == '__main__':
+    main()
