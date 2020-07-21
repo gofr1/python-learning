a5a6f57968cb13aaf7a5377e390260cb53c8653d
Ilia Odintsov <gofr.one@gmail.com>
2020-07-21 14:22:03 +0200

    Pytest commands amend

--- unit_tests/3_execeptions/test_2_cmd.py
+++ unit_tests/3_execeptions/test_2_cmd.py
@@ -1,7 +1,7 @@
 import pytest
 
 # launch this test with:
-# pytest -m "mt1 or mt2"
+# pytest -m "mt1 or mt2" unit_tests/
 
 @pytest.mark.mt1
 def test1():
@@ -12,3 +12,7 @@ def test1():
 def test2():
     print('Executing test2!')
     assert True
+
+# this will run all test2 tests
+# pytest -v -s -k "test2" unit_tests/
+
