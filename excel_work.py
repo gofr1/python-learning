3a2c2f2007054fc7222f381c5befc06b0a48e7b3
Ilia Odintsov <gofr.one@gmail.com>
2020-10-19 12:45:41 +0200

    Setting row height, column width and font styles

--- excel_work.py
+++ excel_work.py
@@ -3,6 +3,7 @@
 
 import openpyxl
 from openpyxl.utils import get_column_letter, column_index_from_string
+from openpyxl.styles import Font
 
 # open file
 wb = openpyxl.load_workbook('example.xlsx')
@@ -123,3 +124,36 @@ sheet['A1'].value
 #* 'Hello world!'
 
 wb.save('example_copy.xlsx')
+
+# setting the font style
+wb = openpyxl.Workbook()
+sheet = wb['Sheet']
+
+fontObj1 = Font(name='Times New Roman', bold=True)
+sheet['A1'].font = fontObj1
+sheet['A1'] = 'Bold Times New Roman'
+
+fontObj2 = Font(size=24, italic=True)
+sheet['B1'].font = fontObj2
+sheet['B1'] = '24 pt Italic'
+
+wb.save('styles.xlsx')
+
+# setting row height and column width
+wb = openpyxl.Workbook()
+sheet = wb.active
+
+sheet['A1'] = 'Tall row'
+sheet['B2'] = 'Wide column'
+
+# the row height can be set to an integer or float value between 0 and 409.
+# the default row height is 12.75
+sheet.row_dimensions[1].height = 50
+
+# the column width can be set to an integer or float value between 0 and 255.
+# the default column width is 8.43 characters. 
+sheet.column_dimensions['B'].width = 30
+
+wb.save('dimensions.xlsx')
+
+
