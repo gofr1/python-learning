2519db565e24c458029041e2a897a61b200f46ce
Ilia Odintsov <gofr.one@gmail.com>
2020-10-24 17:45:32 +0200

    Carrots^ cost dollars$

--- regex_basics.py
+++ regex_basics.py
@@ -178,3 +178,38 @@ alphaNumericRegex.findall('No "punctuation marks" in the results. 11')
 notAlphaNumericRegex = re.compile(r'[^a-zA-Z0-9]')
 notAlphaNumericRegex.findall('No "punctuation marks" in the results. 11')
 #* [' ', '"', ' ', '"', ' ', ' ', ' ', '.', ' ']
+
+# The Caret and Dollar Sign Characters
+
+#  You can also use the caret symbol (^) at the start of a regex to indicate that a match must occur 
+# at the beginning of the searched text. Likewise, you can put a dollar sign ($) at the end of the 
+# regex to indicate the string must end with this regex pattern. 
+#  And you can use the ^ and $ together to indicate that the entire string must match the regex—that 
+# is, it’s not enough for a match to be made on some subset of the string.
+
+beginsWithHello = re.compile(r'^Hello')
+beginsWithHello.search('Hello, world!')
+#* <re.Match object; span=(0, 5), match='Hello'>
+
+beginsWithHello.search('He said hello.') == None
+#* True
+
+# The r'\d$' regular expression string matches strings that end with a numeric character from 0 to 9.
+endsWithNumber = re.compile(r'\d$')
+endsWithNumber.search('Your number is 42')
+#* <re.Match object; span=(16, 17), match='2'>
+
+endsWithNumber.search('Your number is forty two.') == None
+#* True
+
+# The r'^\d+$' regular expression string matches strings that both begin and end with one or more numeric characters.
+wholeStringIsNum = re.compile(r'^\d+$')
+wholeStringIsNum.search('1234567890')
+#* <re.Match object; span=(0, 10), match='1234567890'>
+
+wholeStringIsNum.search('12345xyz67890') == None
+#* True
+wholeStringIsNum.search('12  34567890') == None
+#* True
+
+#! Carrots cost dollars (Caret^ symbol comes first, Dollar$ comes as the end)
