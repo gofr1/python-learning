3a32f5a8c47ad53f46a19f5fb961f00cf0872d9a
Ilia Odintsov <gofr.one@gmail.com>
2020-10-26 12:29:08 +0100

    Managing complex regexes

--- regex_basics.py
+++ regex_basics.py
@@ -313,3 +313,22 @@ namesRegex.sub('CENSORED', 'Agent Alice gave the secret documents to Agent Bob.'
 agentNamesRegex = re.compile(r'Agent (\w)\w*')
 agentNamesRegex.sub(r'\1****', 'Agent Alice told Agent Carol that Agent Eve knew Agent Bob was a double agent.')
 #* 'A**** told C**** that E**** knew B**** was a double agent.'
+
+# Managing Complex Regexes
+
+# instead of a hard-to-read regular expression like this:
+phoneRegex = re.compile(r'((\d{3}|\(\d{3}\))?(\s|-|\.)?\d{3}(\s|-|\.)\d{4}(\s*(ext|x|ext.)\s*\d{2,5})?)')
+
+#you can spread the regular expression over multiple lines with comments like this:
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?            # area code
+    (\s|-|\.)?                    # separator
+    (\d{3})                       # first 3 digits
+    (\s|-|\.)                     # separator
+    (\d{4})                       # last 4 digits
+    (\s*(ext|x|ext.)\s*\d{2,5})?  # extension
+    )''', re.VERBOSE)
+
+mo = phoneRegex.search('My number is 555.123-4567 ext. 456.')
+mo.groups()
+#* ('555.123-4567 ext. 456', '555', '.', '123', '-', '4567', ' ext. 456', 'ext.')
