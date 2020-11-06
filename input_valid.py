a2566c37df4c77fe16d92b9e6625fed4992563ab
Ilia Odintsov <gofr.one@gmail.com>
2020-11-06 14:11:19 +0100

    Input: standard way

---
+++ input_valid.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Srandard way
+while True:
+    print('Enter your age:')
+    age = input()
+    try:
+        age = int(age)
+    except:
+        print('Please use numeric digits.')
+        continue
+    if age < 1:
+        print('Please enter a positive number.')
+        continue
+    break
+
+print(f'Your age is {age}.')
+
+
