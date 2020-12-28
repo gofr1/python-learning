8c8f5fdef2437941e7646fdeca0b3eebe801400a
Ilia Odintsov <gofr.one@gmail.com>
2020-12-28 13:45:07 +0300

    PyAutoGUI getting pixel colours

--- gui/gui_auto.py
+++ gui/gui_auto.py
@@ -7,7 +7,7 @@
 #     sudo apt-get install python3-dev
 # PyAutoGUI install the modules it depends on, including PyTweening, PyScreeze, PyGetWindow, PymsgBox, and MouseInfo.
 
-import pyautogui
+import pyautogui, time
 
 wh = pyautogui.size() # Obtain the screen resolution.
 wh
@@ -49,3 +49,52 @@ p[0] # The x-coordinate is at index 0. y - 1
 #* 756
 p.x # The x-coordinate is also in the x attribute. p.y
 #* 756
+
+# Clicking the mouse
+pyautogui.click(756, 665) # Move mouse to (1756, 665) and click.
+
+# Dragging the mouse
+time.sleep(10)
+pyautogui.click()    # Click to make the window active.
+distance = 300
+change = 20
+while distance > 0:
+    pyautogui.drag(distance, 0, duration=0.2)   # Move right.
+    distance = distance - change
+    pyautogui.drag(0, distance, duration=0.2)   # Move down.
+    pyautogui.drag(-distance, 0, duration=0.2)  # Move left.
+    distance = distance - change
+    pyautogui.drag(0, -distance, duration=0.2)  # Move up.
+
+# if you open gimp and create file this script will draw
+# something like spiral:
+#* -------+
+#* |+----+|
+#* ||+--+||
+#* |||--+||
+#* ||+--+||
+#* |+----+|
+#* +------+
+
+# Scrolling the mouse
+# Passing a positive integer scrolls up, and passing a negative integer scrolls down.
+pyautogui.scroll(50)
+pyautogui.scroll(-50)
+
+
+# Getting a screenshot (require scrot)
+# These functions can also return a Pillow Image object of the current screen’s appearance.
+im = pyautogui.screenshot()
+im
+#* <PIL.PngImagePlugin.PngImageFile image mode=RGB size=1920x1080 at 0x7FE6414E37F0>
+
+# You can obtain the RGB color value of a particular pixel on the screen with the pixel() function.
+pyautogui.pixel(50, 200)
+#* RGB(red=41, green=40, blue=54)
+
+# pixelMatchesColor() function will return True if the pixel at 
+# the given x- and y-coordinates on the screen matches the given color.
+pyautogui.pixelMatchesColor(50, 200, (41, 40, 54))
+#* True
+pyautogui.pixelMatchesColor(50, 200, (100, 54, 24))
+#* False
