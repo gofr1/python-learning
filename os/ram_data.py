4817ba13276c07310e8e5c47e5558d4a568a9197
Ilia Odintsov <gofr.one@gmail.com>
2021-01-17 16:28:06 +0300

    Add nice printing and use class in get current ram usage script

--- os/ram_data.py
+++ os/ram_data.py
@@ -1,42 +1,45 @@
 #!/usr/bin/env python3
 
 import psutil
+from tabulate import tabulate
+
+class MemoryUsageDetails(object):
+    
+    def __init__(self):
+        self.vm = psutil.virtual_memory()
+        self.swap = psutil.swap_memory()
+
+    def get_info(self):
+        # memory details
+        info = [{
+            "Type": "RAM",
+            "Total": get_size(self.vm.total),
+            "Available": get_size(self.vm.available),
+            "Used": get_size(self.vm.used),
+            "Percentage": str(self.vm.percent)
+        },
+        # swap details
+        {
+            "Type": "SWAP",
+            "Total": get_size(self.swap.total),
+            "Available": get_size(self.swap.free),
+            "Used": get_size(self.swap.used),
+            "Percentage": str(self.swap.percent)
+        }]
+        return info
+
+    def __repr__(self):
+        mem = tabulate(self.get_info(), headers="keys", tablefmt="pretty")
+        return mem
+
 
 def get_size(bytes, suffix="B"):
     factor = 1024
     for unit in ["", "K", "M", "G", "T", "P"]:
         if bytes < factor:
-            return f'{bytes:.2f}{unit} {suffix}'
+            return f'{bytes:.2f}{unit}{suffix}'
         bytes /= factor
 
-def ram_info():
-    # memory details
-    vm = psutil.virtual_memory()
-    info = {
-        "Type": "RAM",
-        "Total": get_size(vm.total),
-        "Available": get_size(vm.available),
-        "Used": get_size(vm.used),
-        "Percentage": vm.percent
-    }
-    return info
-
-def swap_info():
-    swap = psutil.swap_memory()
-    swap_info = {
-        "Type": "SWAP",
-        "Total": get_size(swap.total),
-        "Free": get_size(swap.free),
-        "Used": get_size(swap.used),
-        "Percentage": swap.percent
-    }
-    return swap_info
-
-def print_info(info):
-    for key, value in info.items():
-        print(f'{key}: {value}')
-    print("-"*20)
 
 if __name__ == '__main__':
-    print_info(ram_info())
-    print_info(swap_info())
+    print(MemoryUsageDetails())
