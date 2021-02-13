c95a5afb9f0a52cae6264c2f6159a808a2c1c3e6
Ilia Odintsov <gofr.one@gmail.com>
2021-02-13 14:42:26 +0300

    Small changes in few files

--- automation/script.py
+++ automation/script.py
@@ -1,4 +1,7 @@
 import subprocess
 
 for i in range(5):
-    subprocess.check_call(['python3', 'example.py'])
+    subprocess.check_call(['python3', 'automation/example.py'])
+
+r = subprocess.getoutput('date')
+print(f'It is {r}')
