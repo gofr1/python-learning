4d721f740ea2d920217fd9f0dc31b586a8f03834
Ilia Odintsov <gofr.one@gmail.com>
2021-02-15 17:04:37 +0300

    Basic JSON serialization

---
+++ serialization/json/event.py
@@ -0,0 +1,18 @@
+"""Shopping events"""
+event = {
+   'name': 'John Doe',
+   'purchase date': '2021-02-15T10:08:15.561Z',
+   'items': [{
+       'item': 'Beer',
+       'amount': 6,
+       'price': 2.99
+    }, {
+       'item': 'Bread',
+       'amount': 2,
+       'price': 1.95
+    }, {
+       'item': 'Water',
+       'amount': 5,
+       'price': 0.54
+    }]
+}
