0c447348a18acd59ed5fa2407993348525e06b7c
Ilia Odintsov <gofr.one@gmail.com>
2021-08-15 12:38:59 +0300

    Task 18 split image

--- challenge/18_balloons.py
+++ challenge/18_balloons.py
@@ -24,3 +24,21 @@ pic_name = picture_urls[0]
 # and get it
 response = requests.get(f'{standard_url}{pic_name}', auth = HTTPBasicAuth('huge', 'file'))
 img = Image.open(BytesIO(response.content))
+
+# split images
+img_size = img.size
+left_img = Image.new('RGB', (img_size[0]//2,img_size[1]), color = 'white')
+right_img = Image.new('RGB', (img_size[0]//2,img_size[1]), color = 'white')
+
+for y in range(1,img_size[1]-1):
+    for x in range(1,img_size[0]-1):
+        r,g,b = img.getpixel((x,y))
+        if x < img_size[0]//2:
+            left_img.putpixel((x,y),(r,g,b))
+        else:
+            xr = x - img_size[0]//2
+            right_img.putpixel((xr,y),(r,g,b))
+
+# save images to check
+left_img.save('left_img.png')
+right_img.save('right_img.png')
