789bf6c73b96602c4c50d4e7490f47eb91e7c4b6
Ilia Odintsov <gofr.one@gmail.com>
2021-08-16 13:08:43 +0300

    Task 18 search difference

--- challenge/18_balloons.py
+++ challenge/18_balloons.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 from requests.auth import HTTPBasicAuth
 from io import BytesIO
-from PIL import Image
+from PIL import Image, ImageChops 
 
 standard_url = "http://www.pythonchallenge.com/pc/return/"
 url = f"{standard_url}balloons.html"
@@ -24,21 +24,48 @@ pic_name = picture_urls[0]
 # and get it
 response = requests.get(f'{standard_url}{pic_name}', auth = HTTPBasicAuth('huge', 'file'))
 img = Image.open(BytesIO(response.content))
+img.save('orig_img.png')
 
 # split images
 img_size = img.size
-left_img = Image.new('RGB', (img_size[0]//2,img_size[1]), color = 'white')
-right_img = Image.new('RGB', (img_size[0]//2,img_size[1]), color = 'white')
+x_size = img_size[0]//2
+y_size = img_size[1]
 
-for y in range(1,img_size[1]-1):
-    for x in range(1,img_size[0]-1):
+left_img = Image.new('RGB', (x_size,y_size), color = 'white')
+right_img = Image.new('RGB', (x_size,y_size), color = 'white')
+
+for y in range(1,y_size-1):
+    for x in range(1,img.size[0]-1):
         r,g,b = img.getpixel((x,y))
-        if x < img_size[0]//2:
+        if x < x_size:
             left_img.putpixel((x,y),(r,g,b))
         else:
-            xr = x - img_size[0]//2
+            xr = x - x_size 
             right_img.putpixel((xr,y),(r,g,b))
 
 # save images to check
 left_img.save('left_img.png')
 right_img.save('right_img.png')
+
+# find difference
+both_img = Image.new('RGB', (x_size,y_size), color = 'white')
+
+for x in range(1,x_size-1):
+   for y in range(1,y_size-1):
+        rl,gl,bl = left_img.getpixel((x,y))
+        rr,gr,br = right_img.getpixel((x,y))
+        #if x < 20 and y< 20:
+        #    print(rl,gl,bl)
+        #    print(rr,gr,br)
+        r,g,b = (rl-rr), (gl-gr), (bl-br)
+        #print(r,g,b)
+        both_img.putpixel((x,y),(r,g,b))
+
+both_img.save('both_img.png')
+
+# finding difference with ImageChops
+diff = ImageChops.difference(right_img, left_img) 
+  
+# showing the difference 
+diff.show()
+diff.save('both_img.png') 
