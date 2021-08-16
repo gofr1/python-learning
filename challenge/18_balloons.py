7accbe6b64fecdc70581cd54ac5a0ad51aea82ae
Ilia Odintsov <gofr.one@gmail.com>
2021-08-16 13:43:32 +0300

    Task 18 try extrema

--- challenge/18_balloons.py
+++ challenge/18_balloons.py
@@ -26,6 +26,11 @@ response = requests.get(f'{standard_url}{pic_name}', auth = HTTPBasicAuth('huge'
 img = Image.open(BytesIO(response.content))
 img.save('orig_img.png')
 
+# This method is used to get the mode (bands) present in an image.
+print(f'Bands: {Image.Image.getbands(img)}')
+# Gets the minimum and maximum pixel values for each band in the image.
+print(f'Extrema: {Image.Image.getextrema(img)}')
+
 # split images
 img_size = img.size
 x_size = img_size[0]//2
@@ -34,8 +39,8 @@ y_size = img_size[1]
 left_img = Image.new('RGB', (x_size,y_size), color = 'white')
 right_img = Image.new('RGB', (x_size,y_size), color = 'white')
 
-for y in range(1,y_size-1):
-    for x in range(1,img.size[0]-1):
+for y in range(0,y_size):
+    for x in range(0,img.size[0]):
         r,g,b = img.getpixel((x,y))
         if x < x_size:
             left_img.putpixel((x,y),(r,g,b))
@@ -47,25 +52,54 @@ for y in range(1,y_size-1):
 left_img.save('left_img.png')
 right_img.save('right_img.png')
 
-# find difference
+print(f'Bands: {Image.Image.getbands(left_img)}')
+print(f'Extrema: {Image.Image.getextrema(left_img)}')
+
+print(f'Bands: {Image.Image.getbands(right_img)}')
+print(f'Extrema: {Image.Image.getextrema(right_img)}')
+
+# Extrema looks different
+# Let's use extrema diff to cretae a diffrenet picture with delta for each color
+
+left_ex = Image.Image.getextrema(left_img)
+right_ex = Image.Image.getextrema(right_img)
+ex = []
+
+for i in range(3):
+    ex.append(left_ex[i][1] - right_ex[i][1])
+
+print(ex)
+
+# use delta 
+ex_img = Image.new('RGB', (x_size,y_size), color = 'white')
+
+for x in range(0,x_size):
+   for y in range(0,y_size):
+        rl,gl,bl = right_img.getpixel((x,y))
+        r,g,b = (rl+ex[0]), (gl+ex[0]), (bl+ex[2])
+        #print(r,g,b)
+        ex_img.putpixel((x,y),(r,g,b))
+
+ex_img.save('ex_img.png') 
+
+
+# find difference in manual way with loops
 both_img = Image.new('RGB', (x_size,y_size), color = 'white')
 
-for x in range(1,x_size-1):
-   for y in range(1,y_size-1):
+for x in range(0,x_size):
+   for y in range(0,y_size):
         rl,gl,bl = left_img.getpixel((x,y))
         rr,gr,br = right_img.getpixel((x,y))
-        #if x < 20 and y< 20:
-        #    print(rl,gl,bl)
-        #    print(rr,gr,br)
         r,g,b = (rl-rr), (gl-gr), (bl-br)
         #print(r,g,b)
         both_img.putpixel((x,y),(r,g,b))
 
-both_img.save('both_img.png')
+both_img.save('both_img.png') 
 
-# finding difference with ImageChops
+# let's try finding difference with ImageChops
 diff = ImageChops.difference(right_img, left_img) 
-  
-# showing the difference 
-diff.show()
+#diff = diff.convert('RGBA')
+
+# showing & saving the difference 
+# diff.show()
 diff.save('both_img.png') 
