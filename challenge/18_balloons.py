bf18207d12ad12ae2988729c31012b4bc7bc5658
Ilia Odintsov <gofr.one@gmail.com>
2021-08-19 10:22:43 +0300

    Task 18 solved

--- challenge/18_balloons.py
+++ challenge/18_balloons.py
@@ -127,7 +127,7 @@ with open(gz_filename, 'wb') as fout:
 # Now we have a file
 import gzip
 
-with gzip.open(gz_filename, 'rb') as f:
+with gzip.open(gz_filename, 'r') as f:
     file_content = f.read()
 
 for string in str(file_content).split('\\n'):
@@ -155,9 +155,6 @@ with open('sample_right.png', 'wb') as fout:
 
 # But guess it is not the thing, images are incomplete
 # Let's try to check the differences and write in different files
-# 1 that is in left but not in right
-# 2 file is in right but not in left
-# 3 are in both files
 
 bytes_left = []
 bytes_right = []
@@ -165,8 +162,8 @@ i=0
 
 for string in str(file_content).split('\\n'):
     len_string = len(string)
-    bytes_string_left = string[0:len_string//2].strip() 
-    bytes_string_right = string[-len_string//2:].strip()
+    bytes_string_left = string[:len_string//2].strip() 
+    bytes_string_right = string[len_string//2:].strip()
     if i == 0:
         bytes_string_left = bytes_string_left[2:]
     i+=1
@@ -177,17 +174,22 @@ bytes_all = zip(bytes_left[:-1], bytes_right[:-1])
 bytes_one = ''
 bytes_two = ''
 bytes_three = ''
-
+# 1 that is in left but not in right
+# 2 file is in right but not in left
+# 3 are in both files
 for ba in bytes_all:
     if ba[0] == ba[1]:
-        bytes_three += ba[0] + ' ' + ba[1] +' '
+        bytes_three += ba[0] + ' '
+        bytes_one += ba[0] + ' ' 
+        bytes_two += ba[1] + ' ' 
     else:
-        if ba[0] == '':
-            bytes_one += ba[1] +' '
-            bytes_two += ba[1] + ' '
-        else:           
-            bytes_one += ba[0] + ' ' + ba[1] +' '
-            bytes_two += ba[1] + ' ' + ba[0] + ' '
+        #if ba[0] == '':
+        #    bytes_two += ba[1] + ' '
+        #elif ba[1] == '':
+        #    bytes_one += ba[0] +' '
+        #else:           
+        bytes_one += ba[0] + ' ' 
+        bytes_two += ba[1] + ' ' 
         #print(f'"{ba[0]}","{ba[1]}')
 
 with open('1.png', 'wb') as fout:
@@ -198,3 +200,58 @@ with open('2.png', 'wb') as fout:
 
 with open('3.png', 'wb') as fout:
     fout.write(bytearray.fromhex(bytes_three))
+
+# Not that good
+# Let's use difflib
+
+data = gzip.open(gz_filename)
+left_part, right_part = [], []
+
+for string in data:
+    left_part.append(string[:53].decode()+"\n")
+    right_part.append(string[56:].decode())
+
+data.close()
+
+import difflib
+
+#? Each line of a Differ delta begins with a two-letter code:
+#? 
+#? '- ' line unique to sequence 1
+#? '+ ' line unique to sequence 2
+#? '  ' line common to both sequences
+#? '? ' line not present in either input sequence
+
+compare = difflib.Differ().compare(left_part, right_part)
+
+# check what is inside
+from pprint import pprint
+
+pprint(list(compare))
+
+# '  2f 17 a4 5d 8e 74 86 c9 02 d3 98 e6 09 62 99 43 ae 50\n',
+# '+ 1a 0c 06 83 11 a8 c1 60 30 18 81 1a 0c 06 83 11 a8 c1\n',
+# '  d5 54 89 24 56 8e ac 46 2d b1 d9 a2 ce 38 4d 91 30 78\n',
+# '- 08 84 40 05 02 81 40 08 54 20 10 08 84 40 05 02 81 40\n',
+# '  86 49 8d fa cb 3a 79 2b 2d da 30 50 14 45 51 60 60 7c\n',
+
+f1 = open('1.png', 'wb')
+f2 = open('2.png', 'wb')
+f3 = open('3.png', 'wb')
+
+for line in compare:
+    bs = bytes([int(p, 16) for p in line[2:].strip().split(' ') if p])
+    if line[0] == '+':
+        f2.write(bs)
+    elif line[0] == '-':
+        f3.write(bs)
+    else:
+        f1.write(bs)
+
+f1.close()
+f2.close()
+f3.close()
+
+# ../hex/bin.html - back one level (from "return") and go "hex/bin.html")
+# butter - login
+# fly - password
