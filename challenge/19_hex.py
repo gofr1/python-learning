90ffa11f5b35eb5a6c2f025fe876cd9e46c53a21
Ilia Odintsov <gofr.one@gmail.com>
2021-08-20 09:25:22 +0300

    Task 19 save and play the wav file

--- challenge/19_hex.py
+++ challenge/19_hex.py
@@ -14,3 +14,28 @@ print(str(page_contents)[0:643])
 
 #* Content-type: audio/x-wav; name="indian.wav"
 #* Content-transfer-encoding: base64
+
+# And further goes base64 string
+
+import base64
+
+base64_message = str(page_contents)[645:151431]
+base64_bytes = base64_message.encode('utf8')
+message_bytes = base64.b64decode(base64_bytes)
+
+with open('indian.wav','wb') as fout:
+    fout.write(message_bytes)
+
+# Play the file
+from playsound import playsound
+playsound('indian.wav')
+
+# wav file says 'sorry!'
+# Lets try sorry.html
+
+url = f"{standard_url}sorry.html"
+response = requests.get(url, auth = HTTPBasicAuth('butter', 'fly'))
+page_contents = BeautifulSoup(response.text, 'html.parser')
+
+print(page_contents)
+#* - "what are you apologizing for?"
