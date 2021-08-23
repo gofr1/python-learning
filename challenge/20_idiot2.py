50e9395f8aad60b79ecf2bc6e674270b179d5240
Ilia Odintsov <gofr.one@gmail.com>
2021-08-23 09:50:58 +0300

    Task 20 start

---
+++ challenge/20_idiot2.py
@@ -0,0 +1,26 @@
+#20
+from bs4 import BeautifulSoup
+import requests
+from requests.auth import HTTPBasicAuth
+
+standard_url = "http://www.pythonchallenge.com/pc/hex/"
+url = f"{standard_url}idiot2.html"
+response = requests.get(url, auth = HTTPBasicAuth('butter', 'fly'))
+page_contents = BeautifulSoup(response.text, 'html.parser')
+
+print(page_contents)
+
+#* <img border="0" src="unreal.jpg"/>
+#* but inspecting it carefully is allowed.
+
+# get the image
+pic_name = page_contents.find_all('img')[0].get('src')
+
+# and get it
+response = requests.get(f'{standard_url}{pic_name}', auth = HTTPBasicAuth('butter', 'fly'))
+
+# we have 500 server error...
+print(response.status_code)
+
+#with open(f'{pic_name}', 'wb') as fout:
+#    fout.write(response.content)
