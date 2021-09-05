098abf3d870d1c36df27e031dba9519f88e7158d
Ilia Odintsov <gofr.one@gmail.com>
2021-09-05 10:59:22 +0300

    Task 20 content range

--- challenge/20_idiot2.py
+++ challenge/20_idiot2.py
@@ -19,11 +19,11 @@ pic_name = page_contents.find_all('img')[0].get('src')
 # and get it
 response = requests.get(f'{standard_url}{pic_name}', auth = HTTPBasicAuth('butter', 'fly'))
 
-# we have 500 server error...
+#* 200 !!
 print(response.status_code)
 
-#with open(f'{pic_name}', 'wb') as fout:
-#    fout.write(response.content)
+with open(f'{pic_name}', 'wb') as fout:
+    fout.write(response.content)
 
 # Check cookies
 session = requests.Session()
@@ -44,3 +44,20 @@ print(response.headers)
 # The problem with picture is fixed!
 # Hmm... we got 'Content-Range': 'bytes 0-30202/2123456789'
 # Let's investigate further
+
+headers = {"Range": "bytes=30203-30236"}
+response = requests.get(f'{standard_url}{pic_name}', auth = HTTPBasicAuth('butter', 'fly'),headers=headers)
+print(response.headers)
+#* {
+#*     'Content-Type': 'application/octet-stream', 
+#*     'Content-Transfer-Encoding': 'binary', 
+#*     'Content-Range': 'bytes 30203-30236/2123456789', 
+#*     'Content-Length': '34', 
+#*     'Date': 'Sun, 05 Sep 2021 07:54:47 GMT', 
+#*     'Server': 'lighttpd/1.4.55'
+#* }
+
+with open(f'bin.txt', 'wb') as fout:
+    fout.write(response.content)
+# In the file you will see this message:
+#* Why don't you respect my privacy?
