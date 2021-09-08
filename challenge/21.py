cffee418c03b1ab12759b5bb0f7acf226704daf2
Ilia Odintsov <gofr.one@gmail.com>
2021-09-08 11:14:29 +0300

    Task 21 read pack file

--- challenge/21.py
+++ challenge/21.py
@@ -8,7 +8,7 @@ with ZipFile('bin.zip', 'r') as zout:
 #* ['readme.txt', 'package.pack']
 
 with ZipFile('bin.zip', 'r') as zout:
-    print(zout.read('readme.txt', pwd=bytes('invader'[::-1].encode())).decode('utf8'))
+    print(zout.read('readme.txt', pwd=bytes('invader'[::-1].encode())).decode('utf-8'))
 
 #* Yes! This is really level 21 in here. 
 #* And yes, After you solve it, you'll be in level 22!
@@ -18,3 +18,7 @@ with ZipFile('bin.zip', 'r') as zout:
 #* * We used to play this game when we were kids
 #* * When I had no idea what to do, I looked backwards.
 
+with ZipFile('bin.zip', 'r') as zout:
+    print(zout.read('package.pack', pwd=bytes('invader'[::-1].encode())).decode('ISO-8859-1'))
+# UnicodeDecodeError: 'utf-8' codec can't decode byte 0x9c in position 1: invalid start byte
+# Let's change to some different
